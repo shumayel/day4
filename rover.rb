7471fb1b52f9bff0932f598b8d02230f35261f10
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-11 18:14:28 -0500

    Update 2

--- rover.rb
+++ rover.rb
@@ -8,40 +8,56 @@ def initialize(x, y, direction)
     end
 
     def read_instruction(movement)
-    if movement = "L"
-        turn_left
-    elsif movement = "R"
-        turn_right
-    elsif movement = "M"
-        move
-    else
-        puts "Invalid instructions."
+
+        movement.each do |m|
+            if m == "L"
+                turn_left
+            elsif m == "R"
+                turn_right
+            elsif m == "M"
+                move
+            else
+                puts "Invalid instructions."
+            end
+        end
     end
 
-    def move
+    def move(somevalue)
         # if facing north want to move y - 1
-        if COMPASS = "N"
-            y = -1
-        elsif COMPASS = "E"
-            y = 1
-        elsif COMPASS = "S"
-            y = 1
-        elsif COMPASS = "W"
-            y = 1
+        if COMPASS == "N"
+            @y += 1
+        elsif COMPASS == "E"
+            @x += 1
+        elsif COMPASS == "S"
+            @y -= 1
+        elsif COMPASS == "W"
+            @x += 1
+        else
+            puts "Not a direction!"
+        end
     end
 
-    def turn_left
-        if direction = 0
-            direction = 3
-   end
+    def turn_left(derp)
+        if direction == 0
+            direction == 3
+        end
+
         @direction -= 1
     end
 
     def turn_right
-        if direction = 3
-            direction = 0
+        if direction == 3
+            direction == 0
         end
         @direction += 1
     end
-
 end
+
+
+puts "Rover 1 Output"
+rover1 = Rover.new(1, 2, "N")
+rover1.read_instruction(["L", "M", "L", "M", "L", "M", "L", "M", "M"])
+puts ""
+puts "Rover 2 Output"
+rover2 = Rover.new(3, 3, "E")
+rover2.read_instruction(["M", "M", "R", "M", "M", "R", "M", "R", "R", "M"])
